5322621105a0a34d76e6206327070bb396716bb9
AbbasShajarian <abbas.saki.snapp@gmail.com>
2026-02-15 00:32:01 +0330

    Disable TS type check during build on Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ const nextConfig = {
     // Work around build failures caused by ESLint stack overflow on Vercel.
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    // Work around build failures due to type-check stack overflow on Vercel.
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
